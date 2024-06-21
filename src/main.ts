f2021ec65ea440581c03ae26518486a3f6a5770b
laura-benavente <laurabenaventealmazan@gmail.com>
2024-06-21 11:38:42 +0200

    fixed 2

--- src/main.ts
+++ src/main.ts
@@ -1,179 +1,116 @@
 import "./style.css";
-
 console.log("Hello Typescript!");
 let points: number = 0;
-const pointsDisplay = document.getElementById(
-  "pointsDisplay"
-) as HTMLDivElement;
+const pointsDisplay = document.getElementById("pointsDisplay");
 
 function muestraPuntuacion() {
-  pointsDisplay.textContent = `Puntos: ${points}`;
+    if (pointsDisplay !== null && pointsDisplay !== undefined && pointsDisplay instanceof HTMLDivElement) {
+        pointsDisplay.textContent = `Puntos: ${points}`;
+    }
 }
 
 function increasePoints() {
-  points++;
-  muestraPuntuacion();
+    points++;
+    muestraPuntuacion();
 }
 
 function decreasePoints() {
-  if (points > 0) {
-    points--;
-    muestraPuntuacion();
-  }
+    if (points > 0) {
+        points--;
+        muestraPuntuacion();
+    }
 }
 
-export const showMessage = (): void => {
-  let message = "";
-  stayManageButtons();
+export const obtenerEstadoPartida = (points: number) => {
+    let status = '';
+    stayManageButtons();
 
-  switch (true) {
-    case points < 4:
-      message = "Has sido muy conservador";
-      break;
-    case points === 5:
-      message = "Te ha entrado el canguelo eh?";
-      break;
-    case points >= 6 && points <= 7:
-      message = "Casi casi...";
-      break;
-    case points === 7.5:
-      message = "¡ Lo has clavado! ¡Enhorabuena!";
-      break;
-  }
+    if (points < 4) {
+        status = 'Has sido muy conservador';
+    } else if (points === 5) {
+        status = 'Te ha entrado el canguelo eh?';
+    } else if (points >= 6 && points <= 7) {
+        status = 'Casi casi...';
+    } else if (points === 7.5) {
+        status = '¡ Lo has clavado! ¡Enhorabuena!';
+    } else if (points > 7.5) {
+        status = 'Game Over';
+    }
 
-  window.alert(message);
-};
-
-const increaseButton = document.getElementById(
-  "increaseButton"
-) as HTMLButtonElement;
-		
-if (
-  increaseButton !== null &&
-  increaseButton !== undefined &&
-  increaseButton instanceof HTMLButtonElement
-) {
-  increaseButton.addEventListener("click", increasePoints);
-} else {
-  console.log("El boton no existe");
+    return status;
 }
 
-const decreaseButton = document.getElementById(
-  "decreaseButton"
-) as HTMLButtonElement;
+function addEventListenerIfButtonExists(id: string, eventType: string, listener: EventListener) {
+    const element = document.getElementById(id);
 
-if (
-  decreaseButton !== null &&
-  decreaseButton !== undefined &&
-  decreaseButton instanceof HTMLButtonElement
-) {
-  decreaseButton.addEventListener("click", decreasePoints);
-} else {
-  console.log("El boton no existe");
+    if (element !== null && element !== undefined && element instanceof HTMLButtonElement) {
+        element.addEventListener(eventType, listener);
+    } else {
+        console.log(`El botón con id ${id} no existe`);
+    }
 }
 
-const stayButton = document.getElementById("stayButton") as HTMLButtonElement;
-
-if (
-  stayButton !== null &&
-  stayButton !== undefined &&
-  stayButton instanceof HTMLButtonElement
-) {
-  stayButton.addEventListener("click", showMessage);
-} else {
-  console.log("El boton no existe");
-}
-
-const newGameButton = document.getElementById(
-  "newGameButton"
-) as HTMLButtonElement;
-
-if (
-  newGameButton !== null &&
-  newGameButton !== undefined &&
-  newGameButton instanceof HTMLButtonElement
-) {
-  newGameButton.addEventListener("click", newGame);
-} else {
-  console.log("El boton no existe");
-}
-
-
-const nextMoveButton = document.getElementById(
-  "nextMoveButton"
-) as HTMLButtonElement;
-
-if (nextMoveButton !== null &&
-  nextMoveButton !== undefined &&
-  nextMoveButton instanceof HTMLButtonElement) {
-  nextMoveButton.addEventListener("click", handlePideCartaClick);
-} else {
-  console.log("El boton no existe");
-}
+addEventListenerIfButtonExists("increaseButton", "click", increasePoints);
+addEventListenerIfButtonExists("decreaseButton", "click", decreasePoints);
+addEventListenerIfButtonExists("newGameButton", "click", newGame);
+addEventListenerIfButtonExists("nextMoveButton", "click", handlePideCartaClick);
+addEventListenerIfButtonExists("randomCardButton", "click", handlePideCartaClick);
 
 muestraPuntuacion();
 
 function dameCarta(): number {
-  return Math.floor(Math.random() * 10) + 1;
+    return Math.floor(Math.random() * 10) + 1;
 }
-
 
 const obtenerNumeroCarta = (numeroAleatorio: number) => {
-  if (numeroAleatorio > 7) {
-    return numeroAleatorio + 2;
-  }
-  return numeroAleatorio;
+    if (numeroAleatorio > 7) {
+        return numeroAleatorio + 2;
+    }
+    return numeroAleatorio;
 };
 
-function mapearCartaImagen(carta: number): string {
-  switch (carta) {
-    case 1:
-      return "src/assets/images/1_as-copas.jpg";
-    case 2:
-      return "src/assets/images/2_dos-copas.jpg";
-    case 3:
-      return "src/assets/images/3_tres-copas.jpg";
-    case 4:
-      return "src/assets/images/4_cuatro-copas.jpg";
-    case 5:
-      return "src/assets/images/5_cinco-copas.jpg";
-    case 6:
-      return "src/assets/images/6_seis-copas.jpg";
-    case 7:
-      return "src/assets/images/7_siete-copas.jpg";
-    case 10:
-      return "src/assets/images/10_sota-copas.jpg";
-    case 11:
-      return "src/assets/images/11_caballo-copas.jpg";
-    case 12:
-      return "src/assets/images/12_rey-copas.jpg";
-    default:
-      return "src/assets/images/back.jpg";
+
+function message (points: number): string {
+  let message = '';
+
+  if (points < 4) {
+      message = "Has sido muy conservador";
+  } else if (points === 5) {
+      message = "Te ha entrado el canguelo eh?";
+  } else if (points >= 6 && points <= 7) {
+      message = "Casi casi...";
+  } else if (points === 7.5) {
+      message = "¡ Lo has clavado! ¡Enhorabuena!";
+  } else if (points > 7.5) {
+      message = "Game Over";
   }
+
+  return message;
 }
+
 function getValueToSum(carta: number): number {
     if (carta > 7) {
-      return 0.5;
+        return 0.5;
     }
     return carta;
 }
 
 const sumarPuntos = (puntos: number) => {
     return points + puntos;
-  };
-  
+};
+
 const actualizarPuntos = (puntosSumados: number) => {
     points = puntosSumados;
 };
 
 function sumarValorCarta(cardNumber: number) {
-  const puntosSumados = sumarPuntos(getValueToSum(cardNumber));
-  actualizarPuntos(puntosSumados)
+    const puntosSumados = sumarPuntos(getValueToSum(cardNumber));
+    actualizarPuntos(puntosSumados);
 
-  muestraPuntuacion();
-  setTimeout(() => {
-    manageFinishGame();
-  }, 1);
+    muestraPuntuacion();
+    setTimeout(() => {
+        manageFinishGame();
+    }, 1);
 }
 
 function manageFinishGame() {
@@ -184,77 +121,97 @@ function manageFinishGame() {
     }
     if (points > 7.5) {
         finishGameManageButtons();
-        console.log("Has perdido la partida")
+        console.log("Has perdido la partida");
         window.alert("Game Over");
-     
     }
 }
-
 
 function mostrarCarta(urlCarta: string) {
     const cartaImagen = document.getElementById("cartaImagen");
     if (
-      cartaImagen !== null &&
-      cartaImagen !== undefined &&
-      cartaImagen instanceof HTMLImageElement
+        cartaImagen !== null &&
+        cartaImagen !== undefined &&
+        cartaImagen instanceof HTMLImageElement
     ) {
-      cartaImagen.src = urlCarta;
+        cartaImagen.src = urlCarta;
     }
 }
 
 function handlePideCartaClick() {
-  const carta = dameCarta();
-  const numeroCarta = obtenerNumeroCarta(carta)
-  const cartaURL = mapearCartaImagen(numeroCarta);
-  mostrarCarta(cartaURL);
-  sumarValorCarta(numeroCarta);
-}
-
-const pideCartaButton = document.getElementById(
-  "randomCardButton"
-) as HTMLButtonElement;
-
-		
-if (
-  pideCartaButton !== null &&
-  pideCartaButton !== undefined &&
-  pideCartaButton instanceof HTMLButtonElement
-) {
-  pideCartaButton.addEventListener("click", handlePideCartaClick);
-} else {
-  console.log("El boton no existe");
+    const carta = dameCarta();
+    const numeroCarta = obtenerNumeroCarta(carta);
+    const cartaURL = mapearCartaImagen(numeroCarta);
+    mostrarCarta(cartaURL);
+    sumarValorCarta(numeroCarta);
 }
 
 function newGame() {
-  points = 0;
-  const cartaURL = mapearCartaImagen(0);
-  mostrarCarta(cartaURL);
-  muestraPuntuacion();
-  newGameManageButtons();
+    points = 0;
+    const cartaURL = mapearCartaImagen(0);
+    mostrarCarta(cartaURL);
+    muestraPuntuacion();
+    newGameManageButtons();
 }
 
 const newGameManageButtons = () => {
-  newGameButton.classList.add("hiden");
-  newGameButton.classList.remove("show");
-  nextMoveButton.classList.add("hiden");
-  nextMoveButton.classList.remove("show");
-  stayButton.classList.add("show");
-  stayButton.classList.remove("hiden");
-  pideCartaButton.disabled = false
+    const newGameButton = document.getElementById("newGameButton");
+    const nextMoveButton = document.getElementById("nextMoveButton");
+    const stayButton = document.getElementById("stayButton");
+    const pideCartaButton = document.getElementById("randomCardButton");
+
+    if (newGameButton && newGameButton instanceof HTMLButtonElement) {
+        newGameButton.classList.add("hiden");
+        newGameButton.classList.remove("show");
+    }
+    if (nextMoveButton && nextMoveButton instanceof HTMLButtonElement) {
+        nextMoveButton.classList.add("hiden");
+        nextMoveButton.classList.remove("show");
+    }
+    if (stayButton && stayButton instanceof HTMLButtonElement) {
+        stayButton.classList.add("show");
+        stayButton.classList.remove("hiden");
+    }
+    if (pideCartaButton && pideCartaButton instanceof HTMLButtonElement) {
+        pideCartaButton.disabled = false;
+    }
 };
 
 const stayManageButtons = () => {
-  newGameButton.classList.add("show");
-  newGameButton.classList.remove("hiden");
-  nextMoveButton.classList.add("show");
-  nextMoveButton.classList.remove("hiden");
-  stayButton.classList.add("hiden");
-  stayButton.classList.remove("show");
+    const newGameButton = document.getElementById("newGameButton");
+    const nextMoveButton = document.getElementById("nextMoveButton");
+    const stayButton = document.getElementById("stayButton");
+
+    if (newGameButton && newGameButton instanceof HTMLButtonElement) {
+        newGameButton.classList.add("show");
+        newGameButton.classList.remove("hiden");
+    }
+    if (nextMoveButton && nextMoveButton instanceof HTMLButtonElement) {
+        nextMoveButton.classList.add("show");
+        nextMoveButton.classList.remove("hiden");
+    }
+    if (stayButton && stayButton instanceof HTMLButtonElement) {
+        stayButton.classList.add("hiden");
+        stayButton.classList.remove("show");
+    }
 };
+addEventListenerIfButtonExists("stayButton", "click", stayManageButtons);
 
 const finishGameManageButtons = () => {
-  newGameButton.classList.add("show");
-  nextMoveButton.classList.add("hiden");
-  stayButton.classList.add("hiden");
-  pideCartaButton.disabled = true
+    const newGameButton = document.getElementById("newGameButton");
+    const nextMoveButton = document.getElementById("nextMoveButton");
+    const stayButton = document.getElementById("stayButton");
+    const pideCartaButton = document.getElementById("randomCardButton");
+
+    if (newGameButton && newGameButton instanceof HTMLButtonElement) {
+        newGameButton.classList.add("show");
+    }
+    if (nextMoveButton && nextMoveButton instanceof HTMLButtonElement) {
+        nextMoveButton.classList.add("hiden");
+    }
+    if (stayButton && stayButton instanceof HTMLButtonElement) {
+        stayButton.classList.add("hiden");
+    }
+    if (pideCartaButton && pideCartaButton instanceof HTMLButtonElement) {
+        pideCartaButton.disabled = true;
+    }
 };
